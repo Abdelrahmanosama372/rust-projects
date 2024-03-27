b0f69528c6e77fefb9d5f0b42067126968744b6c
abdelrahman <abdelrahmanosama372@gmail.com>
2024-03-27 23:45:49 +0200

    Refactor search functions implementation

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -53,25 +53,19 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in content.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    results
+    content
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
+
 }
 
 pub fn search_ignore_case<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
     let query = query.to_lowercase();
-
-    for line in content.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-    results
+    content
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
 
 #[cfg(test)]
