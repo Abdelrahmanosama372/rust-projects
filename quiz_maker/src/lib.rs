1ec711b3e6cf694aabc06bcb248e2dd1d5e7a8c6
abdelrahman <abdelrahmanosama372@gmail.com>
2025-06-10 19:42:45 +0300

    Refactor: replace not needed string allocations

--- quiz_maker/src/lib.rs
+++ quiz_maker/src/lib.rs
@@ -1,17 +1,20 @@
-use colored::*;
-use std::error::Error;
 use std::fs;
 use std::io;
+use std::error::Error;
+use colored::*;
+
 
 pub fn run(args: Vec<String>) -> Result<(), Box<dyn Error>> {
     if args.len() < 2 {
         return Err("Error: file path is missing".into());
     }
 
-    let content = read_file(&args[1])?;
+    let content = read_file(&args[1]);
+    if let Err(err) = content {
+        return Err(err);
+    }
 
-    let lines: Vec<&str> = content.split('\n').filter(|s| !s.is_empty()).collect();
-
+    let lines: Vec<String> = content.unwrap().split('\n').map(String::from).collect();
     let mut correct_ans = 0;
     for question in lines.chunks(6) {
         display_question(question);
@@ -20,18 +23,18 @@ pub fn run(args: Vec<String>) -> Result<(), Box<dyn Error>> {
         let mut usr_ans = String::new();
         io::stdin().read_line(&mut usr_ans).unwrap();
 
-        let res = check_question_answer(usr_ans.as_str(), question[5]);
-        if res {
+        let res = check_question_answer(&usr_ans,&question[5]);
+        if res == true {
             correct_ans += 1;
-            println!("{}", "correct".green());
-        } else {
-            println!("{}", "false".red());
+            println!("{}","correct".green());
+        }else {
+            println!("{}","false".red());
         }
     }
 
-    println!("{}", "------------------------------");
-    println!("{} {}/{}", "your score:", correct_ans, lines.len() / 6);
-    println!("{}", "------------------------------");
+    println!("{}","------------------------------");
+    println!("{} {}/{}","your score:",correct_ans,lines.len()/6);
+    println!("{}","------------------------------");
 
     Ok(())
 }
@@ -41,16 +44,16 @@ fn read_file(file_path: &str) -> Result<String, Box<dyn Error>> {
     Ok(content)
 }
 
-fn display_question(question: &[&str]) {
-    println!("{}", question[0].magenta());
+fn display_question(question: &[String]) -> () {
+    println!("{}",question[0].magenta());
     for str in &question[1..5] {
-        println!("{}", str.bright_cyan());
+        println!("{}",str.bright_cyan());
     }
 }
 
-fn check_question_answer(usr_ans: &str, answer: &str) -> bool {
-    if answer.starts_with(usr_ans.trim()) {
-        return true;
+fn check_question_answer(usr_ans: &String,answer: &String) -> bool {
+    if usr_ans.starts_with(answer) {
+        return true; 
     }
-    false
+    return false;
 }
